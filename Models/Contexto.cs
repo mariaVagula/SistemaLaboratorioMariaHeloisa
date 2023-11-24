e82e290fd936dc72a7040ec82661fbb08971bbaa
mariaVagula <mariavaguladec@gmail.com>
2023-11-24 08:32:30 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SistemaLaboratorio.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Paciente> Paciente { get; set;}
+        //public DbSet<Profissional> Profissional { get; set;}
+        //public DbSet<Cargo> Cargo { get; set;}
+        //public DbSet<Estado> Estado { get; set;}
+        //public DbSet<Cidade> Cidade { get; set;}
+        //public DbSet<Resultado> Resultado { get; set;}
+        //public DbSet<Exame> Exame { get; set;}
+
+    }
+}
